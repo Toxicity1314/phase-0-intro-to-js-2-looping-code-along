41cf8d9426299bd3261688526088f7e63c31a5b0
Sean Welsh <s.welsh1314@gmail.com>
2022-10-12 20:43:35 -0400

    commit tree 1972f264cc1f85b23dc7c245aab108f11941afbb

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++){
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts){
+    for (let i= 0; i , i < gifts.length; i++){
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+
+    //return gifts;
+}
+function writeCards(name, event){
+    const newArray =[]
+    for (let i=0; i < name.length; i++){
+        newArray.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+
+    }
+    return newArray
+}
+wrapGifts(gifts);
+console.log(writeCards(["charlie", "Samip", "Ali"], "birthday"))
+
+function countDown(num){
+    while(num >=0){
+        console.log(num)
+        num--
+    }
+}
+countDown(10)
